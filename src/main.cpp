02fda6a58ea8c169371205da1ee9383e2e9c232b
Meirlan <aitmuhamet.meirlan28@mail.ru>
2024-07-10 02:31:00 +0500

    Added GLAD library

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,58 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
-int main(void)
+int main()
 {
     GLFWwindow* window;
 
-    /* Initialize the library */
+    /* Инициализация GLFW */
     if (!glfwInit())
+    {
+        std::cerr << "Ошибка при инициализации GLFW" << std::endl;
         return -1;
+    }
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    /* Создание окна */
+    window = glfwCreateWindow(800, 600, "Пример GLFW", NULL, NULL);
     if (!window)
     {
+        std::cerr << "Ошибка при создании окна GLFW" << std::endl;
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
+    /* Делаем текущим контекст окна */
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
+    /* Инициализация GLAD */
+    if (!gladLoadGL())
+    {
+        std::cerr << "Не удалось загрузить GLAD!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Вывод версии OpenGL */
+    std::cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << std::endl;
+
+    glClearColor(0, 1, 0, 1); // Жёлтый фон
+
+    /* Основной цикл программы */
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
-        //glClear(GL_COLOR_BUFFER_BIT);
+        /* Отрисовка здесь */
 
-        /* Swap front and back buffers */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Обмен буферов */
         glfwSwapBuffers(window);
 
-        /* Poll for and process events */
+        /* Обработка событий */
         glfwPollEvents();
     }
 
+    /* Завершение работы GLFW */
     glfwTerminate();
     return 0;
 }
