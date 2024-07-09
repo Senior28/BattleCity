a7493bd70181de5c543c0034badf1a2daabc79d0
Meirlan <aitmuhamet.meirlan28@mail.ru>
2024-07-10 03:23:04 +0500

    OpenGL and glfw initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,79 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
 #include <iostream>
 
-int main()
-{
-    GLFWwindow* window;
+int g_windowSizeX = 960;
+int g_windowSizeY = 540;
+bool g_fullscreen = false;
 
-    /* Инициализация GLFW */
+void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int height)
+{
+    g_windowSizeX = width;
+    g_windowSizeY = height;
+    glViewport(0, 0, g_windowSizeX, g_windowSizeY);
+}
+
+void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(pWindow, GL_TRUE);
+    }
+
+}
+
+int main(void)
+{
+    /* Initialize the library */
     if (!glfwInit())
     {
-        std::cerr << "Ошибка при инициализации GLFW" << std::endl;
+        std::cout << "glfwInit failed!" << std::endl;
         return -1;
     }
 
-    /* Создание окна */
-    window = glfwCreateWindow(800, 600, "Пример GLFW", NULL, NULL);
-    if (!window)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    /* Create a windowed mode window and its OpenGL context */
+    GLFWwindow* pWindow = glfwCreateWindow(g_windowSizeX, g_windowSizeY, "Battle City", nullptr, nullptr);
+    if (!pWindow)
     {
-        std::cerr << "Ошибка при создании окна GLFW" << std::endl;
+        std::cout << "glfwCreateWindow failed!" << std::endl;
         glfwTerminate();
         return -1;
     }
 
-    /* Делаем текущим контекст окна */
-    glfwMakeContextCurrent(window);
+    glfwSetWindowSizeCallback(pWindow, glfwWindowSizeCallback);
+    glfwSetKeyCallback(pWindow, glfwKeyCallback);
 
-    /* Инициализация GLAD */
+    /* Make the window's context current */
+    glfwMakeContextCurrent(pWindow);
+
     if (!gladLoadGL())
     {
-        std::cerr << "Не удалось загрузить GLAD!" << std::endl;
-        glfwTerminate();
-        return -1;
+        std::cout << "Can't load GLAD!" << std::endl;
     }
 
-    /* Вывод версии OpenGL */
-    std::cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << std::endl;
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
-    glClearColor(0, 1, 0, 1); // Жёлтый фон
+    glClearColor(1, 0, 0, 1);
 
-    /* Основной цикл программы */
-    while (!glfwWindowShouldClose(window))
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(pWindow))
     {
-        /* Отрисовка здесь */
-
+        /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Обмен буферов */
-        glfwSwapBuffers(window);
+        /* Swap front and back buffers */
+        glfwSwapBuffers(pWindow);
 
-        /* Обработка событий */
+        /* Poll for and process events */
         glfwPollEvents();
     }
 
-    /* Завершение работы GLFW */
     glfwTerminate();
     return 0;
 }
